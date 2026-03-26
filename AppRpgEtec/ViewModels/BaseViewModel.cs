8f7f06d499468e20fd96d04bbb7568dc79970345
Victor-Picciutti <victorpicciuttidossantos@gmail.com>
2026-03-25 22:23:54 -0300

    Add project files.

---
+++ AppRpgEtec/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AppRpgEtec.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string propertyName = "")//tudo isso é o estalo pra view model ativar e trazer ou pegar a informacao
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); //o ?. significa difernete de nulo
+        }
+    }
+}
