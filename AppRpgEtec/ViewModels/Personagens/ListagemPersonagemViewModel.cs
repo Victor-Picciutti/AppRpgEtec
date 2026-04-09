f4d5dc33329c056f1f133ec6e7067125706f23b3
Victor-Picciutti <victorpicciuttidossantos@gmail.com>
2026-04-08 21:14:21 -0300

    getall

---
+++ AppRpgEtec/ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,37 @@
+﻿using AppRpgEtec.Models;
+using AppRpgEtec.Services.Personagens;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> Personagens { get; set; }
+        public ListagemPersonagemViewModel() 
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+
+            _ = ObterPersonagens();
+        }
+
+        public async Task ObterPersonagens()
+        {
+            try //Junto com o cacth evitara que erros fechem o aplicativo
+            {
+                Personagens = await pService.GetPersonagensAsync();
+                OnPropertyChanged(nameof(Personagens));//Informara a View que houve carregamento
+            }
+            catch (Exception ex)
+            {
+                //Captará o erro para exibir em tela
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+    }
+}
--- AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
+++ AppRpgEtec/ViewModels/Usuarios/UsuarioViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using AppRpgEtec.Models;
 using AppRpgEtec.Services.Usuarios;
+using AppRpgEtec.Views.Personagens;
 using AppRpgEtec.Views.Usuarios;
 using System;
 using System.Collections.Generic;
@@ -84,7 +85,7 @@ namespace AppRpgEtec.ViewModels.Usuarios
 
                     await Application.Current.MainPage.DisplayAlert("Informação", mensagem, "ok");
 
-                    Application.Current.MainPage = new MainPage();
+                    Application.Current.MainPage = new ListagemView();
                 }
             }
             catch (Exception ex)
